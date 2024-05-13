6c19873f8e5324e82501bd699ca74d6a6aca8d5c
zu0909 <jd2814964@gmail.com>
2024-05-13 08:44:05 +0700

    Pesan commit Anda di sini

--- database/migrations/2024_05_11_132333_antrian.php
+++ database/migrations/2024_05_11_132333_antrian.php
@@ -16,6 +16,7 @@ return new class extends Migration
         $table->string('no_antrian');
         $table->string('status_antrian');
         $table->unsignedBigInteger('pasien_id');
+        $table->unsignedBigInteger('dokter_id');
         $table->foreign('dokter_id')->references('id')->on('dokter');
         $table->foreign('pasien_id')->references('id')->on('pasien');
         });
